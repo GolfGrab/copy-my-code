4f8d77ed47aa29b091a1f6e70c829c6cfeadfda5
GolfGrab <surapus01@gmail.com>
2022-12-07 18:03:37 +0700

    add tailwind plugin > tailwind-highlightjs > daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [{
+    pattern: /hljs+/,
+  }],
   theme: {
+    hljs: {
+      theme: 'atom-one-dark-reasonable',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-highlightjs'),
+    require("daisyui")
+  ],
 };
