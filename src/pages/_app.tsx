994971c19482821ca082445aff9de966f8a1a4d3
GolfGrab <surapus01@gmail.com>
2022-12-08 10:25:20 +0700

    add theme and config theme > next-theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -9,9 +9,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider defaultTheme="dracula">
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,4 +14,7 @@ module.exports = {
     require('tailwind-highlightjs'),
     require("daisyui")
   ],
+  daisyui: {
+    darkTheme: "dracula",
+  },
 };
